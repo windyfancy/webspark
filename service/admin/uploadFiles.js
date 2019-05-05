f9870662b5ddb288506a0f0e2155f6998b603245
windy <windyfancy9@126.com>
2019-05-05 18:49:35 +0800

    更新上传目录

--- service/admin/uploadFiles.js
+++ service/admin/uploadFiles.js
@@ -1,8 +1,9 @@
 var path=require("path")
 module.exports= {  
     onRequest() {  
-        var fileName=path.basename(this.request.files.file.path);
-      
+        //var fileName=path.basename(this.request.files.file.path);
+        var root=path.resolve(this.config.uploadDir);
+        var fileName=path.relative(root,this.request.files.file.path).replace("\\","/");
         this.render({fileName:fileName});
     }
   }

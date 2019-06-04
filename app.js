f02b714a5ab39970fdfdd59b0d172bfa84aa907c
windy <windyfancy9@126.com>
2019-06-04 10:50:19 +0800

    首次运行数据库恢复

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var fs = require("fs");
+//首次运行，恢复备份的sqlite数据库
+if(!fs.existsSync("./database/spark.db") && fs.existsSync("./database/spark.db.bak")){
+  fs.copyFileSync("./database/spark.db.bak","./database/spark.db");
+}
+
 var WebContext=require("webcontext");
 var app=new WebContext();
 

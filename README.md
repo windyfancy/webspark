5a346ecd26aa521886e335bc69668d2f89eba785
windy <windyfancy9@126.com>
2019-06-05 02:05:50 +0800

    首次运行自动copy文件

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # webspark
-node.js blog system
+使用node.js开发的博客系统，基于webcontext框架，支持多种数据库，包括mysql,sqlite，默认使用sqlite数据库。本博客系统使用npm安装后即可运行，开箱即用，不需要编写代码，也不需要配置，无其它依赖的服务。
 
- 
+安装
+```
+ npm install --save webspark
+```
+
+首次运行
+
+ ```
+  node -e "require('webspark')"
+ ```
+
+访问：http://localhost/
+
+
+
+后续运行app.js即可
+ ```
+  node app.js
+ ```
---
+++ run.js
@@ -0,0 +1,34 @@
+﻿const fs = require('fs');
+const path = require('path');
+const copyFile = function (srcPath, tarPath, filter = []) {
+    let files=fs.readdirSync(srcPath) 
+ 
+    files.forEach(function (filename) {
+        let filedir = path.join(srcPath, filename);
+        console.log("copy初始文件：",filedir)
+        let stats = fs.statSync(filedir)
+        let isFile = stats.isFile()
+        if (isFile) {                                    // 复制文件
+            const destPath = path.join(tarPath, filename);
+            fs.copyFileSync(filedir, destPath, (err) => { })
+        } else {                                        // 创建文件夹
+            let tarFiledir = path.join(tarPath, filename);
+            if(!fs.existsSync(tarFiledir)){
+                fs.mkdirSync(tarFiledir, (err) => { });
+            }
+            copyFile(filedir, tarFiledir, filter)                 // 递归
+        }
+
+    })
+         
+    
+}
+ 
+if (__dirname.indexOf("node_modules") >= 0) {
+    copyFile(path.resolve(__dirname,'./'), path.resolve(__dirname,'../..'));
+    setImmediate(()=>{
+        require(path.resolve(__dirname,"../../app.js"));
+    })
+    
+    
+}
